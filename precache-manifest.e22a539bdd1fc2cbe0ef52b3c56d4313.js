baa2a36182dbc46c0253e2d883ed4a9fc01cef23
Taylor <UkrainianEagle@q.com>
2019-03-25 21:54:14 -0500

    Updates

---
+++ precache-manifest.e22a539bdd1fc2cbe0ef52b3c56d4313.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/MemoryGame/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "fb083a730414ee73c157",
+    "url": "/MemoryGame/static/js/runtime~main.989f6c97.js"
+  },
+  {
+    "revision": "f7f9ab88da9673bb898d",
+    "url": "/MemoryGame/static/js/main.05132581.chunk.js"
+  },
+  {
+    "revision": "ccdf85a272b151ebd001",
+    "url": "/MemoryGame/static/js/2.7b7f7f25.chunk.js"
+  },
+  {
+    "revision": "f7f9ab88da9673bb898d",
+    "url": "/MemoryGame/static/css/main.5f9ce8ad.chunk.css"
+  },
+  {
+    "revision": "5a1a8ab39e16addcf598d12853450819",
+    "url": "/MemoryGame/index.html"
+  }
+];
